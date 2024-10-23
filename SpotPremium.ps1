530449380e3109a175c8ea7400a377d8b8139070
AJ-Holzer <150062966+AJ-Holzer@users.noreply.github.com>
2024-10-23 21:53:56 +0200

    Delete SpotPremium.ps1

--- SpotPremium.ps1
+++
@@ -1,9 +0,0 @@
-try {
-    Invoke-WebRequest -useb https://raw.githubusercontent.com//spicetify//cli//main//install.ps1 | Invoke-Expression
-    Invoke-WebRequest -useb https://raw.githubusercontent.com//spicetify//marketplace//main//resources//install.ps1 | Invoke-Expression
-}
-catch {
-    Write-Host "An error occurred: $_"
-    Write-Host "Press any key to continue..."
-    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-}
